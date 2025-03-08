02d08f0e32e9117a9441740d2f5e8f8b7cbae747
Alvaro lenon <713alvarolenon@gmail.com>
2025-03-07 23:42:44 -0300

    deixando o jogo funcional, exibindo numero de tentativas e se o numero secreto é maior ou menor que o chute

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
-
+let tentativas = 1
 //let titulo = document.querySelector('h1');
 //titulo.innerHTML = 'Jogo do numero secreto';
 
@@ -15,8 +15,24 @@ function exibirTextoNaTela(tag, Texto){
 exibirTextoNaTela('h1', 'jogo do numero secreto');
 exibirTextoNaTela('p', 'escolha um numero entre 1 a 10');
 
+
 function verificarChute() {
-   console.log(numeroSecreto);
+   let chute = document.querySelector('input').value;
+
+   if (chute == numeroSecreto){
+      exibirTextoNaTela('h1','ACERTOU!');
+      let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+      let mensagemTentativas = `voce descobriu o numero secreto com ${tentativas} ${palavraTentativa}`;
+      exibirTextoNaTela('p', mensagemTentativas);  
+   } else {
+      if (chute > numeroSecreto) {
+         exibirTextoNaTela('p', 'O numero secreto é menor');
+      } else {
+         exibirTextoNaTela('p', 'O numero secreto é maior');
+      }
+      tentativas++;
+   }
+   console.log(chute == numeroSecreto);
 }
 
 function gerarNumeroAleatorio() {
